f72aab85c07235ed566ee8b43b51c49fb0ba3d26
OpenShift guest <gourav@aristontek.com>
2013-06-23 21:50:45 -0400

    added rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuApp::Application.config.secret_token = 'efdf8fa97025529e72ccfc2a702267c15d42429f5cb64e85e7ffb5c41fe0b5d733f1d53f6f2f7be36a59dd70afdfc6b1ca7688de3c12b34609895816bf2174e3'
